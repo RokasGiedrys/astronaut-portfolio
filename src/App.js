70a92bf47a42dc1cd57c2134b7574d0d00599538
Rokas Giedrys <giedrysrokas@gmail.com>
2021-09-14 19:09:20 +0100

    from now on, to reduce boiler plate the nav options have their own component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/header.component";
 import HomePage from "./pages/home-page/home-page.component";
 import PortfolioPage from "./pages/portfolio-page/portfolio-page.component";
 import ContactPage from "./pages/contact-page/contact-page.component";
+import MobileMenuPage from "./pages/mobile-menu-page/mobile-menu-page.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path='/' component={HomePage} />
         <Route exact path='/portfolio' component={PortfolioPage} />
         <Route exact path='/contact' component={ContactPage} />
+        <Route exact path='/menu' component={MobileMenuPage} />
       </Switch>
     </div>
   );
